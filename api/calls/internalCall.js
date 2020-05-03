f9a5b4b33d35cd6854613f9b7438a5b8f10cba80
Jjsue <jjsue_95@hotmail.es>
2020-05-03 13:20:24 +0200

    Todo preparado para internacionalizar

--- api/calls/internalCall.js
+++ api/calls/internalCall.js
@@ -6,6 +6,9 @@ async function adCall(queryParams) {
     return axios({
         method: 'GET',
         url: `http://127.0.0.1:3000/anuncios${queryParams}`,
+        headers: {
+            auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWFjMTZhODRjNTQ0NTMzZWM3Yjg0ZjUiLCJpYXQiOjE1ODg1MDM5NzMsImV4cCI6MTU4ODY3Njc3M30.vofTCRqOcP79f3gVu3gqK3vD-n6XETaqIRHvaE6ce8I'
+        },
     })
         .then(function (response) {
             return response.data;
